39c36f1cb06cd69ae3205b171c779d8a83c8c04f
“Zhu Chen” <znchen720@gmail.com>
2017-10-05 19:35:38 -0400

    Remove title from index.html and use local label

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  constructor(translate: TranslateService) {
+  constructor(translate: TranslateService, private title:Title) {
     translate.setDefaultLang('en');
     translate.use('en');
+    translate.get('title').subscribe((res: string) => {
+      title.setTitle(res);
+    });
   }
 }
